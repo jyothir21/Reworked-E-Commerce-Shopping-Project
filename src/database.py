e2df3910ec2606f2aee4cf411ae7a12a5bec732d
Jyothir Krishnan <kris.jyothir@gmail.com>
2024-05-06 11:47:05 -0400

    finished functionality to add products into the database from the API

--- src/database.py
+++ src/database.py
@@ -1,13 +1,16 @@
 import os
 import sqlite3
 
+# Database class to handle all database operations required
 class Database():
+    # constructor to create the database
     def __init__(self, reset = False):
         if (reset == True):
             os.remove('customer.db')
             
         self.db = sqlite3.connect('customer.db')
-        
+    
+    # method to create tables for database
     def create_tables(self):
         self.db.execute("""CREATE TABLE IF NOT EXISTS Products
                             ( PRODUCT_ID    INTEGER NOT NULL,
@@ -23,17 +26,29 @@ class Database():
                         """)
         
         self.db.commit()
-        
+    
+    # method to set the values in the database from the FakeStoreAPI
     def __setitem__ (self, table, values):
         if (table == "Products"):
+            # Unpack the rating dictionary into separate items
+            rating = values.pop('rating')
+            values.update(rating)
+
+            # Create placeholders and columns strings
             placeholders = ','.join(['?']*len(values))
             columns = ','.join(['PRODUCT_ID', 'PRODUCT_NAME', 'PRODUCT_PRICE', 
                                 'PRODUCT_DESC', 'PRODUCT_CAT', 'PRODUCT_IMG', 
                                 'PRODUCT_RATING', 'PRODUCT_RATING_COUNT'
                                 ])
+
+            # Create the query string
             query = f"INSERT INTO {table}({columns}) VALUES ({placeholders})"
-            self.db.execute("""
-                            """)
+
+            # Execute the query
+            self.db.execute(query, list(values.values()))
+
+        self.db.commit()
     
+    # destructor method to close the database
     def __del__(self):
         self.db.close()
--- src/main.py
+++ src/main.py
@@ -1,18 +1,37 @@
 import requests
 import json
 import database
+import argparse
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-#* Connecting to the Fake Store API and loading the products data
+# Connecting to the Fake Store API and loading the products data
 product_data = requests.get('https://fakestoreapi.com/products')
 print("Connection Response:", product_data.status_code)
 
-#* Getting the JSON format of the data
+# Getting the JSON format of the data
 json_data = product_data.json()
-# print("JSON Data:\n", json.dump(json_data, indent=4))
+#? print("JSON Data:\n", json.dump(json_data, indent=4))
 
-for data in json_data:    
-    print(data["title"])
-    print(data["rating"]["rate"])
+# Create the parser and parse the arguments
+parser = argparse.ArgumentParser(description="Open the database for the server")
+parser.add_argument('--reset', type=bool, default=False,
+                    help='A boolean to decide whether to reset the database')
+args = parser.parse_args()
 
-db = database.Database()
-db.create_tables()
+# Opening database for server with reset value from command line
+db = database.Database(args.reset)
+
+# Reset the database and upload the API information
+if args.reset:
+    db.create_tables()
+    
+    for data in json_data:    
+        db["Products"] = (data)
+    print("Database reset successfully...")
+
+class Handler (BaseHTTPRequestHandler):
+    def do_POST(self):
+        print("Post method")
+        
+    def do_GET(self):
+        print("Get method")
